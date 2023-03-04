3f7407a4ff67502d5736e62b395f788dc5aea159
Rubens Tanioka <87711124+rutanioka@users.noreply.github.com>
2023-03-04 06:32:46 -0300

    06:32

---
+++ .vscode/app/app.py
@@ -0,0 +1,16 @@
+from Flask import Flask
+from .routes.usuario import usuario
+from .routes.produto import produto
+from .commands.userCommand import userCommands
+from .extentions import database
+
+def  create_app (config_object = "app.settings"):
+    app=Flask(__name__)
+    app.config.from_object(config_object)
+    database.init_app(app)
+
+    app.register_blueprint(usuario)
+    app.register_blueprint(produto)
+    app.register_blueprint(userCommands)
+
+    return app
